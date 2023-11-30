0c57ac3d734a0cd2744bc0191d174442ea8a22f7
sesigler <sageess98@gmail.com>
2023-11-30 16:56:18 -0600

    added fun/syn counts for gnomAD/megan's data

--- code/gene_info_counts.R
+++ code/gene_info_counts.R
@@ -7,7 +7,7 @@ library(dplyr)
 library(tidyr)
 library(data.table)
 
-Pop = 'NFE'
+Pop = 'AFR'
 p_exp = 100
 i = 1
 maf = 0.01
@@ -17,10 +17,7 @@ dir = paste0('C:/Users/sagee/Documents/HendricksLab/mastersProject/input/gene_in
 leg = read.table(paste0(dir, 'chr19.block37.', Pop, '.sim', i, '.', p_exp, 'fun.', p_exp, 'syn.legend'), header=T, sep='\t')
 leg$row = 1:nrow(leg)
 
-# meg_leg = read.table(paste0(dir, 'chr19.block37.', Pop, '.sim', i, '.copy.legend'), header=T, sep='\t')
-# leg %>% filter(AC!='.' & AC!=0) %>% group_by(gene) %>% count()
-# meg_leg %>% filter(AC!='.' & AC!=0) %>% group_by(gene) %>% count()
-
+### RAREsim variants
 hap = fread(paste0(dir, 'chr19.block37.', Pop, '.sim', i, '.all.', p_exp, 'fun.', p_exp, 'syn.haps.gz'))
 hap = as.data.frame(hap)
 
@@ -35,3 +32,8 @@ hap2 %>% filter(af > maf) %>% group_by(gene) %>% count()
 
 # See how many fun/syn variants there are by gene
 fun = hap2 %>% group_by(gene, fun) %>% count()
+
+### gnomAD variants
+meg_leg = read.table(paste0(dir, 'chr19.block37.', Pop, '.sim', i, '.copy.legend'), header=T, sep='\t')
+meg_leg %>% filter(AC!='.' & AC!=0) %>% group_by(gene) %>% count()
+meg_leg_fun = meg_leg %>% filter(AC!='.' & AC!=0) %>% group_by(gene, fun) %>% count()
