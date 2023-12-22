fa39115c8c320a5ef190ab0698ca2655ccb803e5
sesigler <sageess98@gmail.com>
2023-12-22 16:42:13 -0600

    updated for rare variants

--- code/plotting_code/plot_unadj_adj_afs.R
+++ code/plotting_code/plot_unadj_adj_afs.R
@@ -22,26 +22,36 @@ dir = 'C:/Users/sagee/Documents/GitHub/masters_project/Data/admixed/AFR_NFE_pops
 dir_out = 'C:/Users/sagee/Documents/GitHub/masters_project/Results/maf_mac_plots/'
 
 prop_ests = read.table(paste0(dir, "T1e_cc_prop_ests_", int_prune, "_v_", ext_prune, "_", Pop1, '_', Pop2, "_", scen, "_maf", maf, ".txt"), header = T)
-counts = read.table(paste0(dir, "T1e_MACs_MAFs_", int_prune, "_v_", ext_prune, "_", Pop1, '_', Pop2, "_", scen, "_maf", maf, ".txt"), header = T)
+# counts = read.table(paste0(dir, "T1e_MACs_MAFs_", int_prune, "_v_", ext_prune, "_", Pop1, '_', Pop2, "_", scen, "_maf", maf, ".txt"), header = T)
+# Counts filtered to rare variants
+counts_adj = read.table(paste0(dir, "T1e_MACs_MAFs_adj_", int_prune, "_v_", ext_prune, "_", Pop1, '_', Pop2, "_", scen, "_maf", maf, ".txt"), header = T)
+counts_unadj = read.table(paste0(dir, "T1e_MACs_MAFs_unadj_", int_prune, "_v_", ext_prune, "_", Pop1, '_', Pop2, "_", scen, "_maf", maf, ".txt"), header = T)
 
-counts = counts %>% mutate(maf_case_unadj_cc = case_maf - unadj_cc_maf,
-                           maf_case_adj_cc = case_maf - adj_cc_maf)
+counts_adj = counts_adj %>% mutate(maf_case_adj_cc = case_maf - adj_cc_maf)
+counts_unadj = counts_unadj %>% mutate(maf_case_unadj_cc = case_maf - unadj_cc_maf)
+
+same_id = intersect(counts_adj$id, counts_unadj$id)
+same_id_2 = intersect(counts_unadj$id, counts_adj$id)
+
+same_adj = counts_adj %>% filter(id %in% same_id)
+same_unadj = counts_unadj %>% filter(id %in% same_id_2)
 
 pairs(counts[, c(1, 3, 5)])
 plot(prop_ests$maf_nfe, prop_ests$maf_afr)
 
 # Boxplot of difference in MAF between cases and external controls
-unadj = data.frame(group = 'Unadjusted External Controls', value = counts$maf_case_unadj_cc)
-adj = data.frame(group = 'Adjusted External Controls', value = counts$maf_case_adj_cc)
+unadj = data.frame(group = 'Unadjusted External Controls', value = counts_unadj$maf_case_unadj_cc)
+adj = data.frame(group = 'Adjusted External Controls', value = counts_adj$maf_case_adj_cc)
 boxplot_data = rbind(unadj, adj)
 boxplot_data$group <- as.factor(boxplot_data$group)
 
 p0 <- ggplot(boxplot_data, aes(x=group, y=abs(value), fill=group)) +
-  geom_boxplot() +
-  labs(y = '|Case AF - External Control AF|', x='Group', title = 'Sceanrio 2: Boxplots of Difference in MAFs between Cases and External Controls \nCases: 100% AFR, External Controls: 80% AFR 20% NFE \nMAF=0.001') +
-  theme_bw(base_size = 18)
+        geom_boxplot() +
+        # scale_y_continuous(limits = c(-0.0001, 0.0001)) +
+        labs(y = '|Case AF - External Control AF|', x='Group', title = 'Sceanrio 2: Boxplots of Difference in MAFs between Cases and External Controls \nCases: 100% AFR, External Controls: 80% AFR 20% NFE \nMAF=0.001') +
+        theme_bw(base_size = 18)
 p0
-ggsave(file = paste0(dir_out, 'boxplot_case_cc_maf_', Pop1, '_', Pop2, '_', scen, '_', folder, '_', int_prune, '_v_', ext_prune, '_maf', maf, '.jpg'), 
+ggsave(file = paste0(dir_out, 'boxplot_rare_case_cc_maf_abs_diff_', Pop1, '_', Pop2, '_', scen, '_', folder, '_', int_prune, '_v_', ext_prune, '_maf', maf, '.jpg'), 
        plot = p0, height = 8, width = 16, units = 'in')
 
 # Scatter plot matrix of AFs
@@ -59,47 +69,49 @@ ggsave(file = paste0(dir_out, 'mac_matrix_', Pop1, '_', Pop2, '_', scen, '_', fo
        plot = p2, height = 8, width = 16, units = 'in')
 
 # Scatter plot by gene of case vs adj cc MACs
-p3 <- ggplot(counts, aes(x=adj_cc_mac, y=case_mac, color=gene)) +
+p3 <- ggplot(counts_adj, aes(x=adj_cc_mac, y=case_mac, color=gene)) +
         geom_point() +
         geom_abline(slope = 1, intercept= 0) +
         facet_wrap(~gene) +
-        labs(y = 'Case MAC', x= 'External Control MAC (Adjusted)', title = 'Scenario 2: Scatter Plot of Case vs Adjusted External Control AFs \nCases: 100% AFR, External Controls: 80% AFR 20% NFE \nMAF=0.001') +
+        labs(y = 'Case MAC', x= 'External Control MAC (Adjusted)', title = 'Scenario 2: Scatter Plot of Case vs Adjusted External Control MACs \nCases: 100% AFR, External Controls: 80% AFR 20% NFE \nMAF=0.001') +
         theme_bw(base_size = 18)
 p3
-ggsave(file = paste0(dir_out, 'mac_case_v_adj_cc_', Pop1, '_', Pop2, '_', scen, '_', folder, '_', int_prune, '_v_', ext_prune, '_maf', maf, '.jpg'), 
+ggsave(file = paste0(dir_out, 'rare_mac_case_v_adj_cc_', Pop1, '_', Pop2, '_', scen, '_', folder, '_', int_prune, '_v_', ext_prune, '_maf', maf, '.jpg'), 
        plot = p3, height = 8, width = 16, units = 'in')
 
 # Scatter plot by gene of case vs adj cc MAFs
-p4 <- ggplot(counts, aes(x=adj_cc_maf, y=case_maf, color=gene)) +
+p4 <- ggplot(counts_adj, aes(x=adj_cc_maf, y=case_maf, color=gene)) +
         geom_point() +
         geom_abline(slope = 1, intercept= 0) +
-        # facet_wrap(~gene) +
-        labs(y = 'Case AF', x= 'External Control AF (Adjusted)', title = 'Scatter Plots of Case vs Adjusted External Control AFs by Gene \nScenario 2: Cases 100% AFR, External Controls 80% AFR 20% NFE \nMAF=0.001') +
-        theme_bw(base_size = 18)
+        facet_wrap(~gene) +
+        labs(y = 'Case AF', x= 'External Control AF (Adjusted)', title = 'Scenario 2: Scatter Plot of Case vs Adjusted External Control MAFs \nCases: 100% AFR, External Controls: 80% AFR 20% NFE \nMAF=0.001') +
+        theme(axis.text.x = element_text(angle = 35, hjust=0.65)) 
+        # theme_bw(base_size = 18)
 p4
-ggsave(file = paste0(dir_out, 'maf_case_v_adj_cc_', Pop1, '_', Pop2, '_', scen, '_', folder, '_', int_prune, '_v_', ext_prune, '_maf', maf, '.jpg'), 
+ggsave(file = paste0(dir_out, 'rare_maf_case_v_adj_cc_', Pop1, '_', Pop2, '_', scen, '_', folder, '_', int_prune, '_v_', ext_prune, '_maf', maf, '.jpg'), 
        plot = p4, height = 8, width = 16, units = 'in')
 
 # Scatter plot by gene of case vs unadj cc MACs
-p5 <- ggplot(counts, aes(x=unadj_cc_mac, y=case_mac, color=gene)) +
+p5 <- ggplot(counts_unadj, aes(x=unadj_cc_mac, y=case_mac, color=gene)) +
         geom_point() +
         geom_abline(slope = 1, intercept= 0) +
-        # facet_wrap(~gene) +
-        labs(y = 'Case MAC', x= 'External Control MAC (Unadjusted)', title = 'Scenario 2: Scatter Plot of Case vs Unadjusted External Control AFs \nCases: 100% AFR, External Controls: 80% AFR 20% NFE \nMAF=0.001') +
+        facet_wrap(~gene) +
+        labs(y = 'Case MAC', x= 'External Control MAC (Unadjusted)', title = 'Scenario 2: Scatter Plot of Case vs Unadjusted External Control MACs \nCases: 100% AFR, External Controls: 80% AFR 20% NFE \nMAF=0.001') +
         theme_bw(base_size = 18)
 p5
-ggsave(file = paste0(dir_out, 'mac_case_v_unadj_cc_', Pop1, '_', Pop2, '_', scen, '_', folder, '_', int_prune, '_v_', ext_prune, '_maf', maf, '.jpg'), 
+ggsave(file = paste0(dir_out, 'rare_mac_case_v_unadj_cc_', Pop1, '_', Pop2, '_', scen, '_', folder, '_', int_prune, '_v_', ext_prune, '_maf', maf, '.jpg'), 
        plot = p5, height = 8, width = 16, units = 'in')
 
 # Scatter plot by gene of case vs unadj cc MAFs
-p6 <- ggplot(counts, aes(x=unadj_cc_maf, y=case_maf, color=gene)) +
+p6 <- ggplot(counts_unadj, aes(x=unadj_cc_maf, y=case_maf, color=gene)) +
         geom_point() +
         geom_abline(slope = 1, intercept= 0) +
-        # facet_wrap(~gene) +
-        labs(y = 'Case AF', x= 'External Control AF (Unadjusted)', title = 'Scatter Plots of Case vs Unadjusted External Control AFs by Gene \nScenario 2: Cases 100% AFR, External Controls 80% AFR 20% NFE \nMAF=0.001') +
-        theme_bw(base_size = 18)
+        facet_wrap(~gene) +
+        labs(y = 'Case AF', x= 'External Control AF (Unadjusted)', title = 'Scenario 2: Scatter Plot of Case vs Unadjusted External Control MAFs \nCases: 100% AFR, External Controls: 80% AFR 20% NFE \nMAF=0.001') +
+        theme(axis.text.x = element_text(angle = 35, hjust=0.65)) 
+        # theme_bw(base_size = 18)
 p6
-ggsave(file = paste0(dir_out, 'maf_case_v_unadj_cc_', Pop1, '_', Pop2, '_', scen, '_', folder, '_', int_prune, '_v_', ext_prune, '_maf', maf, '.jpg'), 
+ggsave(file = paste0(dir_out, 'rare_maf_case_v_unadj_cc_', Pop1, '_', Pop2, '_', scen, '_', folder, '_', int_prune, '_v_', ext_prune, '_maf', maf, '.jpg'), 
        plot = p6, height = 8, width = 16, units = 'in')
 
 # Scatter plot by gene of adj vs unadj cc MACs
@@ -107,7 +119,7 @@ p7 <- ggplot(counts, aes(x=unadj_cc_mac, y=adj_cc_mac, color=gene)) +
         geom_point() +
         geom_abline(slope = 1, intercept= 0) +
         # facet_wrap(~gene) +
-        labs(y = 'External Control MAC (Adjusted)', x= 'External Control MAC (Unadjusted)', title = 'Scenario 2: Scatter Plot of Adjusted External Control vs Unadjusted External Control AFs \nCases: 100% AFR, External Controls: 80% AFR 20% NFE \nMAF=0.001') +
+        labs(y = 'External Control MAC (Adjusted)', x= 'External Control MAC (Unadjusted)', title = 'Scenario 2: Scatter Plot of Adjusted External Control vs Unadjusted External Control MACs \nCases: 100% AFR, External Controls: 80% AFR 20% NFE \nMAF=0.001') +
         theme_bw(base_size = 18)
 p7
 ggsave(file = paste0(dir_out, 'mac_adj_v_unadj_cc_', Pop1, '_', Pop2, '_', scen, '_', folder, '_', int_prune, '_v_', ext_prune, '_maf', maf, '.jpg'), 
@@ -118,7 +130,7 @@ p8 <- ggplot(counts, aes(x=unadj_cc_maf, y=adj_cc_maf, color=gene)) +
         geom_point() +
         geom_abline(slope = 1, intercept= 0) +
         facet_wrap(~gene) +
-        labs(y = 'External Control AF (Adjusted)', x= 'External Control AF (Unadjusted)', title = 'Scatter Plots of Adjusted External Control vs Unadjusted External Control AFs by Gene \nScenario 2: Cases 100% AFR, External Controls 80% AFR 20% NFE \nMAF=0.001') +
+        labs(y = 'External Control AF (Adjusted)', x= 'External Control AF (Unadjusted)', title = 'Scenario 2: Scatter Plot of Adjusted External Control vs Unadjusted External Control MAFs \nCases: 100% AFR, External Controls: 80% AFR 20% NFE \nMAF=0.001') +
         theme_bw(base_size = 18)
 p8
 ggsave(file = paste0(dir_out, 'maf_adj_v_unadj_cc_', Pop1, '_', Pop2, '_', scen, '_', folder, '_', int_prune, '_v_', ext_prune, '_maf', maf, '.jpg'), 
