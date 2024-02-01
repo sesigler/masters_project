5cb02c4a083dd7eaa4e1934dd152912b5e553703
sesigler <sageess98@gmail.com>
2024-02-01 17:20:31 -0600

    added code for adjustment by gene

--- code/typeI_error_code/t1e_gene_2pop.R
+++ code/typeI_error_code/t1e_gene_2pop.R
@@ -155,6 +155,135 @@ for (i in 1:10){
   prox_weighted_int_genes_p = rbind(prox_weighted_int_genes_p, counts_int_wide$prox_w)
   prox_weighted_ext_genes_p = rbind(prox_weighted_ext_genes_p, counts_ext_wide$prox_w)
   prox_weighted_ext_genes_p_adj = rbind(prox_weighted_ext_genes_p_adj, counts_ext_wide_adj$prox_w)
+  
+  ##############################################################################
+  # Adjust AFs at the gene level instead of variant level
+  count_ext_ref <- cbind(count_cases[, c("ac", "af")], cc_refs)
+  colnames(count_ext_ref)[1:4] <- c("ac_case", "af_case", "ac_cc", "af_cc")
+  count_ext_ref = count_ext_ref %>% mutate(row = leg$row, gene = leg$gene, id = leg$id, fun = leg$fun)
+
+  common_ext2 = leg[which((count_ext_ref$af_case > maf & count_ext_ref$af_case < 1-maf) | (count_ext_ref$af_cc > maf & count_ext_ref$af_cc < 1-maf)),]
+
+  count_ext_ref2 = count_ext_ref %>% filter(!(id %in% common_ext2$id))
+
+  names <- c("gene","id", "fun")
+
+  count_ext_ref2 = count_ext_ref2 %>% mutate(across(all_of(names), as.factor))
+
+  data_ext_syn = count_ext_ref2 %>% filter(fun == "syn")
+  data_ext_fun = count_ext_ref2 %>% filter(fun == "fun")
+
+  data_ext_syn2 = data_ext_syn %>% group_by(gene) %>% summarise(af_case = sum(af_case), af_cc = sum(af_cc), af_afr = sum(af_afr), af_nfe = sum(af_nfe),
+                                                                ac_case = sum(ac_case), ac_cc = sum(ac_cc), ac_afr = sum(ac_afr), ac_nfe = sum(ac_nfe))
+
+  adj_AF_syn <- adjAF(data = data_ext_syn2,
+                      reference = c("af_afr", "af_nfe"),
+                      observed = "af_cc",
+                      pi.target = c(case_est_prop[, "af_afr"], case_est_prop[, "af_nfe"]),
+                      pi.observed = c(cc_est_prop[, "af_afr"], cc_est_prop[, "af_nfe"]),
+                      adj_method = "average",
+                      N_reference = c(Nref, Nref),
+                      N_observed = Ncc,
+                      filter = TRUE)
+
+  # Add adj AF to data frame
+  data_ext_syn2$adj_af <- adj_AF_syn$adjusted.AF$adjustedAF
+
+  # Calculate the MINOR adjusted AF
+  data_ext_syn2$adj_maf <- ifelse(data_ext_syn2$adj_af > .5, 1-data_ext_syn2$adj_af, data_ext_syn2$adj_af)
+
+  # Calculate the MINOR adjusted AC
+  data_ext_syn2$adj_mac <- round(data_ext_syn2$adj_maf*(2*Ncc))
+
+  # Same for fun variants
+  data_ext_fun2 = data_ext_fun %>% group_by(gene) %>% summarise(af_case = sum(af_case), af_cc = sum(af_cc), af_afr = sum(af_afr), af_nfe = sum(af_nfe),
+                                                                ac_case = sum(ac_case), ac_cc = sum(ac_cc), ac_afr = sum(ac_afr), ac_nfe = sum(ac_nfe))
+
+  adj_AF_fun <- adjAF(data = data_ext_fun2,
+                      reference = c("af_afr", "af_nfe"),
+                      observed = "af_cc",
+                      pi.target = c(case_est_prop[, "af_afr"], case_est_prop[, "af_nfe"]),
+                      pi.observed = c(cc_est_prop[, "af_afr"], cc_est_prop[, "af_nfe"]),
+                      adj_method = "average",
+                      N_reference = c(Nref, Nref),
+                      N_observed = Ncc,
+                      filter = TRUE)
+
+  # Add adj AF to data frame
+  data_ext_fun2$adj_af <- adj_AF_fun$adjusted.AF$adjustedAF
+
+  # Calculate the MINOR adjusted AF
+  data_ext_fun2$adj_maf <- ifelse(data_ext_fun2$adj_af > .5, 1-data_ext_fun2$adj_af, data_ext_fun2$adj_af)
+
+  # Calculate the MINOR adjusted AC
+  data_ext_fun2$adj_mac <- round(data_ext_fun2$adj_maf*(2*Ncc))
+  
+  # Combine fun and syn data for plotting
+  data_ext_syn2$fun <- "syn"
+  data_ext_fun2$fun <- "fun"
+  data_ext2 <- rbind(data_ext_fun2, data_ext_syn2)
+
+  library(ggplot2)
+  g1 <- ggplot(data_ext2 %>% filter(fun == "fun"), aes(x=adj_maf, y=af_case)) +
+    geom_point() +
+    geom_abline(intercept = 0, slope = 1) +
+    theme_bw() +
+    xlab("Adjusted AF") + ylab("AFR AF") +
+    geom_text(mapping = aes(x = 0.002, y = 0.007),
+              label = paste0("CCC fun = ", round(DescTools::CCC(data_ext_fun2$adj_maf, data_ext_fun2$af_case)$rho.c$est, 6)))
+  g1
+  
+  names <- c("id", "gene", "fun", "case")
+  
+  data.case = data_ext2[, c("gene", "ac_case", "fun")]
+  data.case$case <- "case"
+  colnames(data.case)[2] <- "ac"
+  
+  data.cc = data_ext2[, c("gene", "ac_cc", "fun")]
+  data.cc$case <- "control"
+  colnames(data.cc)[2] <- "ac"
+  
+  data.cc.adj = data_ext2[, c("gene", "adj_mac", "fun")]
+  data.cc.adj$case <- "control"
+  colnames(data.cc.adj)[2] <- "ac"
+  
+  names <- c("gene", "fun", "case")
+  data.prox = data.frame(rbind(data.case, data.cc)) %>% mutate(across(all_of(names), as.factor))
+  data.prox.adj = data.frame(rbind(data.case, data.cc.adj)) %>% mutate(across(all_of(names), as.factor))
+
+  counts.gene = tidyr::pivot_wider(data.prox, names_from=c(case, fun), values_from=ac,
+                                   values_fill=0, names_sep="_")
+  counts.gene.adj = tidyr::pivot_wider(data.prox.adj, names_from=c(case, fun), values_from=ac,
+                                       values_fill=0, names_sep="_")
+  
+  counts.gene = counts.gene %>% mutate(case_ratio = case_fun/case_syn,
+                                       control_ratio = control_fun/control_syn)
+  
+  counts.gene.adj = counts.gene.adj %>% mutate(case_ratio = case_fun/case_syn,
+                                               control_ratio = control_fun/control_syn)
+  
+  # Calculate medians
+  median.case.ratio = median(counts.gene$case_ratio)
+  median.control.ratio = median(counts.gene$control_ratio)
+  
+  median.case.ratio.adj = median(counts.gene.adj$case_ratio)
+  median.control.ratio.adj = median(counts.gene.adj$control_ratio)
+  
+  # Calculate the weighted values and the p-values
+  counts.gene = counts.gene %>% mutate(case_fun_w = case_fun / median.case.ratio,
+                                       control_fun_w = control_fun / median.control.ratio) %>% 
+    mutate(prox = ifelse((case_fun + control_fun < 5) | (case_syn + control_syn < 5), NA, 
+                         proxecat(case_fun, case_syn, control_fun, control_syn)$p.value),
+           prox_w = ifelse((case_fun_w + control_fun_w < 5) | (case_syn + control_syn < 5), NA, 
+                           proxecat(case_fun_w, case_syn, control_fun_w, control_syn)$p.value))
+  
+  counts.gene.adj = counts.gene.adj %>% mutate(case_fun_w = case_fun / median.case.ratio.adj,
+                                               control_fun_w = control_fun / median.control.ratio.adj) %>% 
+    mutate(prox = ifelse((case_fun + control_fun < 5) | (case_syn + control_syn < 5), NA, 
+                         proxecat(case_fun, case_syn, control_fun, control_syn)$p.value),
+           prox_w = ifelse((case_fun_w + control_fun_w < 5) | (case_syn + control_syn < 5), NA, 
+                           proxecat(case_fun_w, case_syn, control_fun_w, control_syn)$p.value))
+  ################################################################################
 
   ### Prep data for other methods
   # convert genotypes into long format for ProxECAT v2
@@ -378,141 +507,6 @@ write.table(burden_ext_genes_p, paste0(dir_out, "T1e_gene_burden_ext_", file_pat
 write.table(burden_all_genes_p, paste0(dir_out, "T1e_gene_burden_all_", file_path), quote=F, row.names=F, col.names=T)
 
 ##############################################################################
-# Adjust AFs at the gene level instead of variant level
-# count_cases2 = count_cases %>% mutate(row = leg$row, gene = leg$gene, id = leg$id, fun = leg$fun)
-# cc_refs2 = cc_refs %>% mutate(row = leg$row, gene = leg$gene, id = leg$id, fun = leg$fun)
-# count_cc2 = count_cc %>% mutate(row = leg$row, gene = leg$gene, id = leg$id, fun = leg$fun)
-# 
-# count_all <- cbind(count_cases2[, c("ac", "af")], cc_refs2)
-# colnames(count_all)[1:4] <- c("ac_case", "af_case", "ac_cc", "af_cc")
-# 
-# # Plot AFR vs NFE AF all variants
-# cc_dat <- CCC(count_all$af_afr, count_all$af_nfe, conf.level = 0.95)
-# lab <- paste("CCC: ", round(cc_dat$rho.c[,1], digits = 3), " (95% CI ",
-#              round(cc_dat$rho.c[,2], digits = 3), " - ",
-#              round(cc_dat$rho.c[,3], digits = 3), ")", sep = "")
-# plot(count_all$af_afr, count_all$af_nfe, xlab = "AFR AF", ylab = "NFE AF")
-# title(main = "AFR vs NFE AF-All Variants")
-# abline(0, 1)
-# text(x = 0.35, y = 0.8, labels = lab)
-# 
-# common_all = leg[which((count_all$af_case > maf & count_all$af_case < 1-maf) | (count_all$af_cc > maf & count_all$af_cc < 1-maf)),]
-# 
-# count_all2 = count_all %>% filter(!(id %in% common_all$id))
-# 
-# # Plot AFR vs NFE AF rare variants
-# cc_dat2 <- CCC(count_all2$af_afr, count_all2$af_nfe, conf.level = 0.95)
-# lab2 <- paste("CCC: ", round(cc_dat2$rho.c[,1], digits = 3), " (95% CI ",
-#              round(cc_dat2$rho.c[,2], digits = 3), " - ",
-#              round(cc_dat2$rho.c[,3], digits = 3), ")", sep = "")
-# plot(count_all2$af_afr, count_all2$af_nfe, xlab = "AFR AF", ylab = "NFE AF")
-# title(main = "AFR vs NFE AF-Rare Variants")
-# abline(0, 1)
-# text(x = 0.002, y = 0.0065, labels = lab2)
-# 
-# # Plot case AF vs Ref AF rare variants
-# plot(count_all2$af_afr, count_all2$af_case, xlab = "AFR AF", ylab = "Case AF")
-# title(main = "Case vs AFR AF-Rare Variants")
-# 
-# plot(count_all2$af_nfe, count_all2$af_case, xlab = "NFE AF", ylab = "Case AF")
-# title(main = "Case vs NFE AF-Rare Variants")
-# 
-# library(ggplot2)
-# library(ggpubr)
-# dir_out = 'C:/Users/sagee/Documents/HendricksLab/admixed/'
-# g1 <- ggplot(data = count_all2, aes(x = af_afr, y = af_case)) +
-#   stat_sum(aes(size = factor(after_stat(n))), geom = "point") +
-#   labs(y='Case AF', x='AFR AF', title = 'Case vs AFR AF-Rare Variants')
-# g1
-# 
-# g2 <- ggplot(data = count_all2, aes(x = af_nfe, y = af_case)) +
-#   stat_sum(aes(size = factor(after_stat(n))), geom = "point") +
-#   labs(y='Case AF', x='NFE AF', title = 'Case vs NFE AF-Rare Variants')
-# g2
-# 
-# case_v_ref_AF <- ggarrange(g1, g2, ncol=2, nrow=1)
-# ggsave(file = paste0(dir_out, 'case_v_ref_AF_rare_variants.jpg'),
-#        plot = case_v_ref_AF, height = 8, width = 15, units = 'in')
-# 
-# g3 <- ggplot(data = count_all2, aes(x = af_afr, y = af_cc)) +
-#   stat_sum(aes(size = factor(after_stat(n))), geom = "point") +
-#   labs(y='Control AF', x='AFR AF', title = 'Control vs AFR AF-Rare Variants')
-# g3
-# 
-# g4 <- ggplot(data = count_all2, aes(x = af_nfe, y = af_cc)) +
-#   stat_sum(aes(size = factor(after_stat(n))), geom = "point") +
-#   labs(y='Control AF', x='NFE AF', title = 'Control vs NFE AF-Rare Variants')
-# g4
-# control_v_ref_AF <- ggarrange(g3, g4, ncol=2, nrow=1)
-# ggsave(file = paste0(dir_out, 'control_v_ref_AF_rare_variants.jpg'),
-#        plot = control_v_ref_AF, height = 8, width = 15, units = 'in')
-# 
-# case_v_ref_AF <- ggarrange(g3, g4, ncol=2, nrow=1)
-# 
-# names <- c("id", "gene", "fun")
-# 
-# count_all2 = count_all2 %>% mutate(across(all_of(names), as.factor))
-# 
-# data_all_syn = count_all2 %>% filter(fun == "syn")
-# data_all_fun = count_all2 %>% filter(fun == "fun")
-# 
-# data_all_syn2 = data_all_syn %>% group_by(gene) %>% summarise(af_case = sum(af_case), af_cc = sum(af_cc), af_afr = sum(af_afr), af_nfe = sum(af_nfe),
-#                                                               ac_case = sum(ac_case), ac_cc = sum(ac_cc), ac_afr = sum(ac_afr), ac_nfe = sum(ac_nfe))
-# 
-# adj_AF_syn <- adjAF(data = data_all_syn2,
-#                     reference = c("af_afr", "af_nfe"),
-#                     observed = "af_cc",
-#                     pi.target = c(pi_tar1, pi_tar2),
-#                     pi.observed = c(cc_est_prop[, "af_afr"], cc_est_prop[, "af_nfe"]),
-#                     adj_method = "average",
-#                     N_reference = c(Nref, Nref),
-#                     N_observed = Ncc,
-#                     filter = TRUE)
-# 
-# # Add adj AF to data frame
-# data_all_syn2$adj_af <- adj_AF_syn$adjusted.AF$adjustedAF
-# 
-# # Calculate the MINOR adjusted AF
-# data_all_syn2$adj_maf <- ifelse(data_all_syn2$adj_af > .5, 1-data_all_syn2$adj_af, data_all_syn2$adj_af)
-# 
-# # Calculate the MINOR adjusted AC
-# data_all_syn2$adj_mac <- round(data_all_syn2$adj_maf*(2*Ncc))
-# 
-# # Same for fun variants
-# data_all_fun2 = data_all_fun %>% group_by(gene) %>% summarise(af_case = sum(af_case), af_cc = sum(af_cc), af_afr = sum(af_afr), af_nfe = sum(af_nfe),
-#                                                               ac_case = sum(ac_case), ac_cc = sum(ac_cc), ac_afr = sum(ac_afr), ac_nfe = sum(ac_nfe))
-# 
-# adj_AF_fun <- adjAF(data = data_all_fun2,
-#                     reference = c("af_afr", "af_nfe"),
-#                     observed = "af_cc",
-#                     pi.target = c(pi_tar1, pi_tar2),
-#                     pi.observed = c(cc_est_prop[, "af_afr"], cc_est_prop[, "af_nfe"]),
-#                     adj_method = "average",
-#                     N_reference = c(Nref, Nref),
-#                     N_observed = Ncc,
-#                     filter = TRUE)
-# 
-# # Add adj AF to data frame
-# data_all_fun2$adj_af <- adj_AF_fun$adjusted.AF$adjustedAF
-# 
-# # Calculate the MINOR adjusted AF
-# data_all_fun2$adj_maf <- ifelse(data_all_fun2$adj_af > .5, 1-data_all_fun2$adj_af, data_all_fun2$adj_af)
-# 
-# data_all_fun2$adj_mac <- round(data_all_fun2$adj_maf*(2*Ncc))
-# 
-# plot(data_all_syn2$af_cc, data_all_syn2$af_case)
-# abline(0, 1)
-# 
-# plot(data_all_syn2$adj_af, data_all_syn2$af_case)
-# abline(0, 1)
-# 
-# plot(data_all_fun2$af_cc, data_all_fun2$af_case)
-# abline(0, 1)
-# 
-# plot(data_all_fun2$adj_af, data_all_fun2$af_case)
-# abline(0, 1)
-
-################################################################################
 # Check adjustments between unadj AF and case AF
 # plot(cc_refs$af, count_cases$af, xlim=c(0, 0.001), ylim = c(0, 0.001))
 # abline(0, 1)
@@ -808,3 +802,52 @@ write.table(burden_all_genes_p, paste0(dir_out, "T1e_gene_burden_all_", file_pat
 # prox_weighted_ext_genes_p = rbind(prox_weighted_ext_genes_p, counts_wide$prox_w)
 # prox_weighted_ext_genes_p_adj = rbind(prox_weighted_ext_genes_p_adj, counts_wide_adj$prox_w)
 ##############################################################################
+# Plot code
+# Plot AFR vs NFE AF rare variants
+# cc_dat2 <- CCC(count_all2$af_afr, count_all2$af_nfe, conf.level = 0.95)
+# lab2 <- paste("CCC: ", round(cc_dat2$rho.c[,1], digits = 3), " (95% CI ",
+#               round(cc_dat2$rho.c[,2], digits = 3), " - ",
+#               round(cc_dat2$rho.c[,3], digits = 3), ")", sep = "")
+# plot(count_all2$af_afr, count_all2$af_nfe, xlab = "AFR AF", ylab = "NFE AF")
+# title(main = "AFR vs NFE AF-Rare Variants")
+# abline(0, 1)
+# text(x = 0.002, y = 0.0065, labels = lab2)
+# 
+# # Plot case AF vs Ref AF rare variants
+# plot(count_all2$af_afr, count_all2$af_case, xlab = "AFR AF", ylab = "Case AF")
+# title(main = "Case vs AFR AF-Rare Variants")
+# 
+# plot(count_all2$af_nfe, count_all2$af_case, xlab = "NFE AF", ylab = "Case AF")
+# title(main = "Case vs NFE AF-Rare Variants")
+# 
+# library(ggplot2)
+# library(ggpubr)
+# dir_out = 'C:/Users/sagee/Documents/HendricksLab/admixed/'
+# g1 <- ggplot(data = count_all2, aes(x = af_afr, y = af_case)) +
+#   stat_sum(aes(size = factor(after_stat(n))), geom = "point") +
+#   labs(y='Case AF', x='AFR AF', title = 'Case vs AFR AF-Rare Variants')
+# g1
+# 
+# g2 <- ggplot(data = count_all2, aes(x = af_nfe, y = af_case)) +
+#   stat_sum(aes(size = factor(after_stat(n))), geom = "point") +
+#   labs(y='Case AF', x='NFE AF', title = 'Case vs NFE AF-Rare Variants')
+# g2
+# 
+# case_v_ref_AF <- ggarrange(g1, g2, ncol=2, nrow=1)
+# ggsave(file = paste0(dir_out, 'case_v_ref_AF_rare_variants.jpg'),
+#        plot = case_v_ref_AF, height = 8, width = 15, units = 'in')
+# 
+# g3 <- ggplot(data = count_all2, aes(x = af_afr, y = af_cc)) +
+#   stat_sum(aes(size = factor(after_stat(n))), geom = "point") +
+#   labs(y='Control AF', x='AFR AF', title = 'Control vs AFR AF-Rare Variants')
+# g3
+# 
+# g4 <- ggplot(data = count_all2, aes(x = af_nfe, y = af_cc)) +
+#   stat_sum(aes(size = factor(after_stat(n))), geom = "point") +
+#   labs(y='Control AF', x='NFE AF', title = 'Control vs NFE AF-Rare Variants')
+# g4
+# control_v_ref_AF <- ggarrange(g3, g4, ncol=2, nrow=1)
+# ggsave(file = paste0(dir_out, 'control_v_ref_AF_rare_variants.jpg'),
+#        plot = control_v_ref_AF, height = 8, width = 15, units = 'in')
+# 
+# case_v_ref_AF <- ggarrange(g3, g4, ncol=2, nrow=1)
