b11a03608e85c3391d1a5503fb902e24f0753443
sesigler <sageess98@gmail.com>
2024-10-17 17:57:22 -0500

    updated file path for Hap and Leg files

--- code/Hapgen/hapgen2_sim100k.sh
+++ code/Hapgen/hapgen2_sim100k.sh
@@ -4,7 +4,7 @@ end=100
 start=$(($end-99))
 
 ### variables to be updated per simulation
-pop=IAM #population: AFR(African), EAS(Eastern Asian), NFE(Non-Finnish European), SAS(South Asian), IAM(Indigenous American)
+pop=EAS #population: AFR(African), EAS(Eastern Asian), NFE(Non-Finnish European), SAS(South Asian), IAM(Indigenous American)
 NE=14269 #effective sample size: AFR(17469), EAS(14269), NFE(11418), SAS(14269)
 Nsim=100000 #number of individuals
 num=19 #chromosome number
@@ -13,7 +13,7 @@ b=37 #block number
 ### input variables
 dir=/home/math/siglersa
 WD=/storage/math/projects/compinfo/simulations
-Hap=${dir}/IAM_haps/master_legend/IAM_chr19_block37_coding_region.hap
+Hap=${WD}/input/${pop}_blocks/${pop}_Block${b}_CDS_ref_added.hap
 Map=${WD}/input/genetic_map_chr${num}_combined.txt
 #DL=$(cat ${WD}/input/${pop}_blocks/${pop}block_disease_loci.txt | sed -n "38p") #38p for NFE
 ### Calculated Disease locus by hand (random non-zero position)
@@ -23,7 +23,7 @@ do
 
 ### output variables
 # Leg is from subset_master.R
-Leg=${dir}/Sim_100k/IAM/subset_master/chr${num}.block${b}.${pop}.sim${i}.legend
+Leg=${dir}/Sim_100k/${pop}/subset_master/chr${num}.block${b}.${pop}.sim${i}.legend
 Output=${dir}/Sim_100k/${pop}/data/chr${num}.block${b}.${pop}.sim${i}
 
 ### save a copy of the legend file
@@ -36,7 +36,7 @@ hapgen2 -h $Hap \
 -l $Leg \
 -o $Output.gz \
 -n $Nsim 0 \
--dl 14884751 1 1.0 1.0 \
+-dl 14673368 1 1.0 1.0 \
 -Ne $NE \
 -no_gens_output
 
